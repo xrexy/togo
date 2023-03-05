eaac27364d88507f1f48b9efe71f079c76bca57a
xrexy <rexydoesimportantthings@gmail.com>
2023-03-05 22:56:57 +0200

    Cleanup vue & add tailwind

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,12 +3,20 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+//       "@components/*": ["./src/components/*"],
+// "@views/*": ["./src/views/*"],
+// "@composables/*": ["./src/composables/*"],
+// "$composables": ["./src/composables"],
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@views': fileURLToPath(new URL('./src/views', import.meta.url)),
+      '@composables': fileURLToPath(new URL('./src/composables', import.meta.url)),
     }
   }
 })
